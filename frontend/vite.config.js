882d7818ebe9939523bf48f04e0c272236926531
irshadaliks786-arch <irshadaliks786@gmail.com>
2026-07-08 19:06:18 +0530

    Add GitHub Pages base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  // GitHub Pages ke liye
+  base: "/smart-health-phc-dashboard/",
+
   server: {
     host: "0.0.0.0",
     port: 5173,
